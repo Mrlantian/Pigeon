b3f06fe5fccf7a222cdcfe6fdb83cc6ceeff6cb8
Gao Hanjie <1114107324@qq.com>
2015-09-04 21:46:09 +0800

    test
    未完成

--- shadowsocks/server.py
+++ shadowsocks/server.py
@@ -1,19 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2015 clowwindy
-#
-# Licensed under the Apache License, Version 2.0 (the "License"); you may
-# not use this file except in compliance with the License. You may obtain
-# a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations
-# under the License.
 
 from __future__ import absolute_import, division, print_function, \
     with_statement
@@ -22,13 +9,15 @@ import sys
 import os
 import logging
 import signal
+import db_transfer
+import time
+import server_pool
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../'))
 from shadowsocks import shell, daemon, eventloop, tcprelay, udprelay, \
     asyncdns, manager
 
 
-def main():
     shell.check_python()
 
     config = shell.get_config(False)
@@ -131,7 +120,14 @@ def main():
             run_server()
     else:
         run_server()
-
+def main():
+    shell.check_python()
+    config = shell.get_config(False)
+    daemon.daemon_exec(config)
+    daemon.set_user(config.get('user', None))
+    thread.start_new_thread(db_transfer.DbTransfer.thread_db, ())
+    while True:
+        time.sleep(99999)
 
 if __name__ == '__main__':
     main()
